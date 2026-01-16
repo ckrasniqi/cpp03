25e2c76406b93ccbd729ac9f56b246a4c1548adf
Clirim Krasniqi <ckrasniq@3-F-11.42heilbronn.de>
2026-01-16 16:49:26 +0100

    Added some more messages for most of the cases

--- ClapTrap.cpp
+++ ClapTrap.cpp
@@ -4,36 +4,45 @@ ClapTrap::ClapTrap() :
 _name("Limi"),
 _hitPoints(10),
 _energyPoints(10),
-_attackDamage(10) {
+_attackDamage(0) {
 	std::cout << "Default constructor called!" << std::endl;
 
 }
 
 void ClapTrap::attack(const std::string &target){
-
-	std::cout << "ClapTrap " << _name << " attacks "
-			  << &target << ", causing " << _attackDamage
-			  << " points of damage!" << std::endl;
-
+	if (_energyPoints >= 1){
+		std::cout << "ClapTrap " << _name << " attacks "
+				  << &target << ", causing " << _attackDamage
+				  << " points of damage!" << std::endl;
+	} else {
+		std::cout << "Claptrap " << _name << " has insufficient amount of Energy points" << std::endl;
+		std::cout << "Amount of Energy points: " << _energyPoints << std::endl;
+	}
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
-
-	std::cout << "ClapTrap " << _name << " loses "
-			  << amount << " hit points."
-			  << std::endl;
-
+	if (_hitPoints > amount){
+		std::cout << "ClapTrap " << _name << " loses "
+				  << amount << " hit points."
+				  << std::endl;
+	} else {
+		std::cout << "ClapTrap " << _name << " loses "
+				  << amount << " hit points and dies in the process! :("
+				  << std::endl;
+	}
 }
 
 void ClapTrap::beRepaired(unsigned int amount){
-
+	if (_energyPoints >= 1){
 	std::cout << "ClapTrap " << _name << " regains "
 			  << amount << " hit points."
 			  << std::endl;
-
+	} else {
+		std::cout << "Claptrap " << _name << " has insufficient amount of Energy points" << std::endl;
+		std::cout << "Amount of Energy points: " << _energyPoints << std::endl;
+	}
 }
 
 ClapTrap::~ClapTrap(){
 	std::cout << "Destructor called!" << std::endl;
-
 }
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "Claptrap.hpp"
+
+int main(){
+
+}
