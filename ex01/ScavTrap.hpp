09284e7584715dd867f4a3012cb9bdc31ed982a0
Clirim Krasniqi <ckrasniq@2-B-8.42heilbronn.de>
2026-01-18 15:52:57 +0100

    So after inheriting the class ClapTrap and its attributes, we create the Orthodox Canonical Form for ScavTrap as well and add the two functions that are special to them.

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -2,16 +2,17 @@
 #define SCAVTRAP_HPP
 
 #include "ClapTrap.hpp"
-#include <string>
-#include <iostream>
 
 class ScavTrap : public ClapTrap{
 	public:
 		ScavTrap();
-		ScavTrap()
+		ScavTrap( std::string name );
+		ScavTrap( const ScavTrap &other );
+		ScavTrap &operator=( const ScavTrap &other );
+		~ScavTrap();
 
-	private:
-
+		void guardGate();
+		void	attack( const std::string& target );
 };
 
 #endif
