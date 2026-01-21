8735857a07e7995b55291741aa4c52b5a2f694ec
Clirim Krasniqi <ckrasniq@3-H-4.42heilbronn.de>
2026-01-21 20:46:51 +0100

    Added more tests and still trying to figure out why i print Default as the name of

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,81 +3,76 @@
 #include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
-
-int main(){
-	// ClapTrap clap;
-	// DiamondTrap diamond;
-	FragTrap frag;
-	ScavTrap scav;
+void printHeader( std::string title ){
+	std::cout << "\n==========================================" << std::endl;
+	std::cout << "         " << title << std::endl;
+	std::cout << "==========================================" << std::endl;
 }
 
-// void printHeader( std::string title ){
-// 	std::cout << "\n==========================================" << std::endl;
-// 	std::cout << "         " << title << std::endl;
-// 	std::cout << "==========================================" << std::endl;
-// }
+int main(){
 
-// int main(){
+	printHeader("CONSTRUCTION CHAINING");
+	{
+		std::cout << "--- Creating a DiamondTrap named 'Diamond' ---" << std::endl;
+		DiamondTrap dia("Diamond");
+	}
 
-// 	printHeader("CONSTRUCTION CHAINING");
-// 	{
-// 		std::cout << "--- Creating a ScavTrap named 'Serena' ---" << std::endl;
-// 		ScavTrap scav("Serena");
-// 	}
+	printHeader("BASIC FUNCTIONALITY & STATS");
+	ScavTrap scav("Scav-V");
+	ClapTrap clap("Clap-C");
+	DiamondTrap dia("Diamond2");
 
-// 	printHeader("BASIC FUNCTIONALITY & STATS");
-// 	ScavTrap scav("Scav-V");
-// 	ClapTrap clap("Clap-C");
-// 	DiamondTrap dia;
+	scav.attack("a training dummy");
+	clap.attack("the same dummy");
+	dia.attack("the wall"); //attacks with 30 points
 
-// 	scav.attack("a training dummy");
-// 	clap.attack("the same dummy");
+	printHeader("SCAVTRAP TESTS");
+	scav.takeDamage(30);
+	scav.beRepaired(10);
+	scav.guardGate();
+	scav.takeDamage(100);
+	scav.guardGate();
 
-// 	scav.takeDamage(30);
-// 	scav.beRepaired(10);
-// 	scav.guardGate();
+	printHeader("DIAMONDTRAP TESTS");
+	dia.takeDamage(99);
+	dia.beRepaired(9);
+	dia.whoAmI();
+	dia.takeDamage(10);
+	dia.whoAmI();
 
-// 	printHeader("FRAGTRAP TESTS");
-// 	FragTrap frag("Fraggy");
-// 	frag.attack("a concrete wall");
-// 	frag.highFivesGuys();
-// 	frag.takeDamage(50);
-// 	frag.beRepaired(20);
-// 	frag.takeDamage(100);
-// 	frag.highFivesGuys();
+	printHeader("FRAGTRAP TESTS");
+	FragTrap frag("Fraggy");
+	frag.attack("a concrete wall");
+	frag.highFivesGuys();
+	frag.takeDamage(50);
+	frag.beRepaired(20);
+	frag.takeDamage(100);
+	frag.highFivesGuys();
 
-// 	printHeader("ENERGY DEPLETION TEST");
-// 	ClapTrap weakling("Weakling");
-// 	std::cout << "Weakling tries to attack 11 times (Energy is 10)..." << std::endl;
-// 	for (int i = 0; i < 11; i++) {
-// 		weakling.attack("the air");
-// 	}
-// 	weakling.beRepaired(5);
+	printHeader("ENERGY DEPLETION TEST");
+	// DiamondTrap (50 energy Points), ScavTrap(50), FragTrap(100)
+	ClapTrap weakling("Weakling");
+	std::cout << "Weakling tries to attack 11 times (Energy is 10)..." << std::endl;
+	for (int i = 0; i < 11; i++) {
+		weakling.attack("the air");
+	}
+	weakling.beRepaired(5);
 
-// 	printHeader("DEATH & RESILIENCE TEST");
-// 	ScavTrap target("TargetBot");
-// 	target.takeDamage(99);
-// 	target.beRepaired(1);
-// 	target.takeDamage(100);
-// 	target.attack("the attacker");
-// 	target.beRepaired(10);
-// 	target.guardGate();
+	printHeader("ORTHODOX CANONICAL FORM TEST");
+	DiamondTrap original("Original");
+	original.takeDamage(50);
 
-// 	printHeader("ORTHODOX CANONICAL FORM TEST");
-// 	ScavTrap original("Original");
-// 	original.takeDamage(50);
+	std::cout << "\n--- Testing Copy Constructor ---" << std::endl;
+	DiamondTrap copy(original);
+	std::cout << "Copy attacks to verify it inherited attributes correctly:" << std::endl;
+	copy.attack("a clone");
 
-// 	std::cout << "\n--- Testing Copy Constructor ---" << std::endl;
-// 	ScavTrap copy(original);
-// 	std::cout << "Copy attacks to verify it inherited attributes correctly:" << std::endl;
-// 	copy.attack("a clone");
+	std::cout << "\n--- Testing Assignment Operator ---" << std::endl;
+	DiamondTrap assigned("Newbie");
+	assigned = original;
+	assigned.whoAmI();
 
-// 	std::cout << "\n--- Testing Assignment Operator ---" << std::endl;
-// 	ScavTrap assigned("Newbie");
-// 	assigned = original;
-// 	assigned.guardGate();
+	printHeader("END OF TESTS - DESTRUCTORS INCOMING");
 
-// 	printHeader("END OF TESTS - DESTRUCTORS INCOMING");
-
-// 	return 0;
-// }
+	return 0;
+}
